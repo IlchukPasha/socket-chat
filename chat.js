eeee703e0d0b161266f19ed9b3acf06463ff9fe4
pasha <returnto001@gmail.com>
2019-12-10 18:39:03 +0200

    Initial commit

---
+++ chat.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer);
+
+io.on('connection', socket => {
+  socket.emit('messageFromServer', { data: 'Welcome!' });
+
+  socket.on('messageToServer', dataFromClient => {
+    console.log(dataFromClient);
+  });
+
+  socket.on('newMessageToServer', message => {
+    io.emit('messageToClients', { text: message.text });
+  });
+});
